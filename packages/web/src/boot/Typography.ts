7e431c91c8db2d3129001e9b0db1a69ab0470644
chaoslee <chaos2lee@qq.com>
2024-11-26 13:06:30 +0800

    documentation: [ADDED] schemas: metadata, section

---
+++ packages/web/src/boot/Typography.ts
@@ -0,0 +1,25 @@
+import { boot } from 'quasar/wrappers';
+
+import Chart from 'components/Typography/Chart.vue';
+import Embed from 'components/Typography/Embed.vue';
+import Figure from 'components/Typography/Figure.vue';
+import Heading from 'components/Typography/Heading.vue';
+import Paragraph from 'components/Typography/Paragraph.vue';
+import Reference from 'components/Typography/Reference.vue';
+import Section from 'components/Typography/Section.vue';
+import Table from 'components/Typography/Table.vue';
+import Title from 'components/Typography/Title.vue';
+
+export default boot(function TypographyLoader({
+	app,
+}) {
+	app.component('TypoChart', Chart);
+	app.component('TypoEmbed', Embed);
+	app.component('TypoFigure', Figure);
+	app.component('TypoHeading', Heading);
+	app.component('TypoParagraph', Paragraph);
+	app.component('TypoReference', Reference);
+	app.component('TypoSection', Section);
+	app.component('TypoTable', Table);
+	app.component('TypoTitle', Title);
+});
